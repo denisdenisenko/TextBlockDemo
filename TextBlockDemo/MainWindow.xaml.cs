be855fdb0f9caea74a9e16ea1d070bb0a098ba9d
Denis <denis.denisenko@gmail.com>
2021-05-27 19:37:46 +0300

    checkboxes

--- TextBlockDemo/MainWindow.xaml.cs
+++ TextBlockDemo/MainWindow.xaml.cs
@@ -45,5 +45,36 @@ namespace TextBlockDemo
             lblCh.Background = Brushes.White;
 
         }
+
+        private void cbParent_Checked(object sender, RoutedEventArgs e)
+        {
+            bool newVal = (cbParent.IsChecked == true);
+            cbOne.IsChecked = newVal;
+            cbTwo.IsChecked = newVal;
+            cbThree.IsChecked = newVal;
+        }
+
+        private void cbParent_Unchecked(object sender, RoutedEventArgs e)
+        {
+            bool newVal = (cbParent.IsChecked == true);
+            cbOne.IsChecked = newVal;
+            cbTwo.IsChecked = newVal;
+            cbThree.IsChecked = newVal;
+        }
+
+        private void cbGroupCheckedChanged(object sender, RoutedEventArgs e) 
+        {
+            cbParent.IsChecked = null;
+            if ((cbOne.IsChecked==true) &&
+                    (cbTwo.IsChecked==true) && (cbThree.IsChecked==true)) 
+            {
+                cbParent.IsChecked = true;
+            }
+            if ((cbOne.IsChecked==false) &&
+                    (cbTwo.IsChecked==false) && (cbThree.IsChecked==false))
+            {
+                cbParent.IsChecked = false;
+            }
+        }
     }
 }
